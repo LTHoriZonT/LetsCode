4907e0c75b51e020abf2befb83a76be7f71d2160
LTHoriZonT <isaquemendes405@gmail.com>
2021-07-05 09:25:06 -0300

    Lacos de repeticao

---
+++ Lets Code fase 2/lacos de repeticao.js
@@ -0,0 +1,27 @@
+//executa um código enquanto uma condição for verdadeira//
+
+let count = 1;
+
+while (count <= 100){
+    console.log(count)
+    count++;
+}
+
+//-------------------------------//
+//do-while > primeiro se executa o código e depois é verificado a condição para continuar executando ou não//
+
+let count1 = 1;
+
+do{
+    console.log(count1)
+    count1++;
+} while(count1 <= 100);
+
+//-------------------------------//
+//estrutura de repetição que é executado um número específico de vezes. Nele é declarado um variável com um valor inicial, depois é determinado a condição de parada e por fim a passo dado entre o valor inicial e o final.//
+
+for(let i = 1; i <= 100; i++){
+    console.log(i);
+}
+
+//------------------------------//
