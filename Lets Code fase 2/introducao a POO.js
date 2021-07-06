aee88495ee27e6356397a758b33a144fdd25929d
LTHoriZonT <isaquemendes405@gmail.com>
2021-07-06 09:45:53 -0300

    introducao a POO, objetos sem classe, atributos e metodos

---
+++ Lets Code fase 2/introducao a POO.js
@@ -0,0 +1,32 @@
+const pessoa = { //objeto sem classe, utilizando quando nao for necessario reaproveita-lo no codigo
+    nome: "Lucas",
+    idade: 26,
+};
+
+console.log(pessoa.nome);
+
+const quadrado = {
+    base: 10,
+    altura: 20,
+
+    calculaArea: function(){
+        return this.base*this.altura
+    }
+};
+
+console.log(quadrado.calculaArea());
+
+const agenda = {
+    contatos:[
+    {nome : "contato1", telefone : "telefone1", email : "email1@teste.com"},
+    {nome : "contato2", telefone : "telefone2", email : "email2@teste.com"},
+    {nome : "contato3", telefone : "telefone3", email : "email3@teste.com"},
+    {nome : "contato4", telefone : "telefone4", email : "email4@teste.com"},
+    ], // atributo
+
+    adicionar: function(contato){
+        this.contatos.push(contato)
+    } // metodo
+};
+
+console.log(agenda.contatos);
