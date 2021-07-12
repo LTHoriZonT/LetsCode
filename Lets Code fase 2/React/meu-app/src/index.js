1520cc5f1161a9a30961c22b44e768ad4122174b
LTHoriZonT <isaquemendes405@gmail.com>
2021-07-12 12:13:46 -0300

    utilizando props

--- Lets Code fase 2/React/meu-app/src/index.js
+++ Lets Code fase 2/React/meu-app/src/index.js
@@ -10,13 +10,18 @@ ReactDOM.render(
       Conteudo
     </App2>
 
-    <App2 title="Titulo2">
-      Conteudo
+    <App2 title="Titulo2"> 
+      Conteudo 
     </App2>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
+/*Qualquer coisa que você colocar entre a abertura e o fechamento das tags do componente serão passados como props.children para o componente! Isso inclui HTML, outros componentes, e até javascript, contanto que esteja entre chaves.
+<App2 title="Titulo2"> 
+Conteudo << aqui
+</App2>
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
