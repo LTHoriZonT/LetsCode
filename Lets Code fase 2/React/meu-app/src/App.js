b6404741011c02901f0debc3219682393d367faa
LTHoriZonT <isaquemendes405@gmail.com>
2021-07-12 10:25:35 -0300

    componente tipo classe

---
+++ Lets Code fase 2/React/meu-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component{
+  render(){
+    return (
+      <>
+        <p>
+        Meu Primeiro paragrafo
+      </p>
+      <p>
+        Meu segundo paragrafo
+      </p>
+      <p>
+        {new Date().toLocaleDateString("pt-br")}
+      </p>
+      </>
+      
+    );
+  }
+}
+
+export default App;
